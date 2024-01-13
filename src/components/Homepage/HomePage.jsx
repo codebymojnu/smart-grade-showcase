0e7567ac564a98a1fb0e8332e9d4c2314dca62d1
mojnuapp <thisismojnu@gmail.com>
2024-01-13 15:39:02 +0600

    Hello 123

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,5 +1,5 @@
 import Footer from "./Footer";
-import HeroSection from "./Herosection";
+import HeroSection from "./HeroSection.jsx";
 import NavigationBar from "./NavigationBar";
 import TableArea from "./TableArea";
 
